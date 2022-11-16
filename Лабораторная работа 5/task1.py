6319c9243fdc73ad34404a28690a4e8c618d81a2
Павлов Р.Е <115102207+pavlovroma@users.noreply.github.com>
2022-11-16 22:02:11 +0300

    Update task1.py

--- Лабораторная работа 5/task1.py
+++ Лабораторная работа 5/task1.py
@@ -1,6 +1,17 @@
 from pprint import pprint
 i = 7
+k = 16
 pop = {
-    'bin': bin(i),'oct': oct(i),'hex': (i) in range(16) }
+    'bin': bin(i),'oct': oct(i),'hex': (i)
+
+}
+
+
 
 pprint(pop)
+pop_=str(pop)
+for i in range(k):
+    print(str(pop) , )
+
+
+
